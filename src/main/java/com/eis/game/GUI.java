bdc69ec094f3bff3d1f0ab573c00f7dd7b09db28
oliboliholzpistoli <93738964+oliboliholzpistoli@users.noreply.github.com>
2023-04-10 14:27:43 +0200

    initial commit

--- src/main/java/com/eis/game/GUI.java
+++ src/main/java/com/eis/game/GUI.java
@@ -1,21 +1,8 @@
 package com.eis.game;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javax.swing.*;
 
-public class GUI extends Application {
+public class GUI {
 
-    @Override
-    public void start(Stage primaryStage){
-        Button btn1 = new Button("Say Hello World");
-        StackPane root = new StackPane();
-        root.getChildren().add(btn1);
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("First JavaFX Application");
-        primaryStage.show();
-    }
+
 }
